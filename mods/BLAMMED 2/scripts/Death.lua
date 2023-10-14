bb7b83c468f78ef6261bcbb9a66f239caf37642b
Z11Coding/Gaming <Zanezack1000@gmail.com>
2023-10-14 11:45:47 -0400

    plans for 0.3.4

--- mods/BLAMMED 2/scripts/Death.lua
+++
@@ -1,9 +0,0 @@
-function onUpdate()
-	if getProperty('boyfriend.curCharacter') == 'bev' then
-		setPropertyFromClass('substates.GameOverSubstate', 'characterName', 'bevdead'); --Character json file for the death animation
-	end
-	if getProperty('boyfriend.curCharacter') == 'bev-colors' then
-		setPropertyFromClass('substates.GameOverSubstate', 'characterName', 'bevdead-colors'); --Character json file for the death animation
-	end
-end
-
